a38bd6f8bf71e604a58d743ee0238908fb0bbc3a
sheep0x <linuxer.sheep.0x@gmail.com>
2014-05-20 00:32:37 +0800

    Polish the code a little bit

--- cs169.1x/homework0p1.rb
+++ cs169.1x/homework0p1.rb
@@ -1,19 +1,45 @@
+# arr.reduce(0, :+) means 0 + arr[0] + arr[1] + ...
 def sum(arr)
-  return 0 if arr.length == 0
-  arr.reduce(:+) 
+  arr.reduce(0, :+)
 end
 
 def max_2_sum(arr)
-  return 0 if arr.length == 0
-  return arr[0] if arr.length == 1
-  sorted = arr.sort { |a, b| b <=> a }
-  sorted[0] + sorted[1]
+  return sum(arr) if arr.length <= 1
+  max2 = arr.sort[-2..-1]
+  max2[0] + max2[1]
 end
 
+#
+# gotcha:
+#   'and' is NOT good for logical operations!  Yes, you can use it; but
+#   better use it for other purpose.  The 'and' and 'or' operators were
+#   actually borrowed from Perl; they are usually used for conditional
+#   executions, although you can use them in expressions if you are careful.
+#   Basically, 'foo and bar' is just another way of saying 'bar if foo'.
+#   For example:
+#       friends.include? 'Suzuka' and print 'Mom, I got a new (girl)friend!'
+#   You'll be surprised if you try something like 'a = b and c': Ruby will
+#   evaluate 'a=b' first, and, if a is true-ish, return c happily.
+#
 def sum_to_n?(arr, n)
-  return true if arr.length == n and n == 0
-  for i in 1..arr.length-1
-    return true if arr[0, i].include? n-arr[i]
-  end
-  return false
+  # I didn't really get it.  If we return true for ([], 0), why don't we
+  # return true for ([1,2,3], 0)?
+  # Btw, I'd be more explicit: return n == 0 if arr.empty?
+  # (some folks prefer n.zero?, though)
+  n == 0 && arr.empty? and return true
+
+  # I'm not sure if you are concerned with performace.  I duplicate the Array
+  # to make the code cleaner.  But it happens to be faster too: duplicating
+  # once and popping n times is more efficient than slicing O(n) times.
+  #arr = arr.dup
+  #while x = arr.pop
+  #  return true if arr.any? {|y| x + y == n }
+  #end
+  #false
+
+  # This is the most expressive way, I guess.  Thanks, Matz!  You can never
+  # imagine how short and expressive you can get in Ruby (at the same time).
+  # Just FYI: Array#combination returns an Enumerator, so don't worry about
+  # the performance.
+  arr.combination(2).any? {|x,y| x + y == n }
 end
